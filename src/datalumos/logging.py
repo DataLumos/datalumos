6cfe45b0e197a2031b1beca18f246fec43faad24
petrakopic <kopicpetra09@gmail.com>
2025-07-29 08:38:03 +0200

    Improve logging setup

---
+++ src/datalumos/logging.py
@@ -0,0 +1,26 @@
+
+import logging
+import os
+from rich.logging import RichHandler
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=os.getenv("LOGLEVEL", "INFO"),
+        format="%(message)s",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
+
+def get_logger(name: str) -> logging.Logger:
+    """Get a logger instance"""
+    return logging.getLogger(name)
+
+
+def setup_output_logger() -> logging.Logger:
+    """Set up a separate logger for formatted analysis outputs"""
+    output_logger = logging.getLogger("datalumos.output")
+    output_handler = logging.StreamHandler()
+    output_handler.setFormatter(logging.Formatter('%(message)s'))
+    output_logger.addHandler(output_handler)
+    output_logger.propagate = False
+    return output_logger
