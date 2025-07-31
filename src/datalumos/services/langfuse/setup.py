3c9439323c4d5b17e73e1df8b14b3201e1a16a8f
dragutin-oreski <dragutin.oreski@gmail.com>
2025-07-31 10:02:20 +0200

    fix: fix env variables and make langfuse client explicit, and ensure langfuse does not need to be used

--- src/datalumos/services/langfuse/setup.py
+++ src/datalumos/services/langfuse/setup.py
@@ -1,32 +1,35 @@
-from langfuse import get_client
+from langfuse import Langfuse
 import nest_asyncio
 import logfire
 
+from datalumos.config import config
 from datalumos.logging import get_logger
 
-logger = get_logger("langfuse")
+logger = get_logger(__name__)
 
 
 def setup_langfuse():
-    langfuse = get_client()
+    langfuse = Langfuse(
+        public_key=config.LANGFUSE_PUBLIC_KEY,
+        secret_key=config.LANGFUSE_SECRET_KEY,
+        host=config.LANGFUSE_HOST
+    )
 
     # Verify connection
     try:
         langfuse.auth_check()
         logger.info("Langfuse client is authenticated and ready!")
+
+        nest_asyncio.apply()
+
+        # Configure logfire instrumentation.
+        logfire.configure(service_name='DataLumos flows', send_to_logfire=False)
+        # This method automatically patches the OpenAI Agents SDK to send logs via OTLP to Langfuse.
+        logfire.instrument_openai_agents()
     except Exception as e:
         logger.warning(
-            f"Authentication failed. Please check your credentials and host. {e}"
+            "Langfuse authentication failed. If you wish to use Langfuse for "
+            "observability, please check your credentials and host."
         )
 
-    nest_asyncio.apply()
-
-    # Configure logfire instrumentation.
-    logfire.configure(
-        service_name='DataLumos flows',
-
-        send_to_logfire=False,
-    )
-    # This method automatically patches the OpenAI Agents SDK to send logs via OTLP to Langfuse.
-    logfire.instrument_openai_agents()
     return langfuse
